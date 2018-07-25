f6bcaef77dae76086898a3589a85b9ac27f49aff
deltakroneker <nikola.milic.ios@gmail.com>
2018-07-25 11:32:11 +0200

    Fixes left side completely

--- base/Controller/ContainerViewController.swift
+++ base/Controller/ContainerViewController.swift
@@ -13,20 +13,25 @@ enum SlideOutState {
     case closed
 }
 
+enum SlideOutSide {
+    case left
+    case right
+}
+
 class ContainerViewController: UIViewController {
 
     var state: SlideOutState = .closed
+    var side: SlideOutSide = .left
     let centerPanelExpandedOffset: CGFloat = 60
-    let expandPointRatio: CGFloat = 0.35
+    let expandPointRatio: CGFloat = 0.5
     let sufficientVelocityForStateChange: CGFloat = 1000
     
-    var leftViewController: SideViewController!
+    var sideViewController: SideViewController!
     var centerNavigationController: UINavigationController!
     var centerViewController: CenterViewController!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         
         centerViewController = UIStoryboard.centerViewController()
         centerViewController.delegate = self
@@ -39,7 +44,6 @@ class ContainerViewController: UIViewController {
         let gesture = UIPanGestureRecognizer(target: self, action: #selector(handlePanGesture(_:)))
         view.addGestureRecognizer(gesture)
     }
-
 }
 
 extension ContainerViewController: CenterViewControllerDelegate {
@@ -47,32 +51,38 @@ extension ContainerViewController: CenterViewControllerDelegate {
     func toggleLeftPanel() {
         let notExpanded = (state != .expanded)
         if notExpanded {
-            addLeftViewController()
+            addSideViewController()
         }
-        animateLeftPanel(shouldExpand: notExpanded)
+        animateSidePanel(shouldExpand: notExpanded)
     }
     
-    func addLeftViewController(){
-        guard leftViewController == nil else { return }
+    func addSideViewController(){
+        guard sideViewController == nil else { return }
         
-        if let vc = UIStoryboard.leftViewController() {
-            leftViewController = vc
+        if let vc = UIStoryboard.sideViewController() {
+            sideViewController = vc
             addChildViewController(vc)
             view.insertSubview(vc.view, at: 0)
         }
     }
     
-    func animateLeftPanel(shouldExpand: Bool){
+    func animateSidePanel(shouldExpand: Bool){
         if shouldExpand {
             state = .expanded
-            let newPosition = centerNavigationController.view.frame.width - centerPanelExpandedOffset
+            var newPosition: CGFloat
+            switch side {
+                case .left:
+                    newPosition = centerNavigationController.view.frame.width - centerPanelExpandedOffset
+                case .right:
+                    newPosition = -centerNavigationController.view.frame.width + centerPanelExpandedOffset
+            }
             animateCenterPanelXPosition(targetPosition: newPosition)
             
         } else {
             animateCenterPanelXPosition(targetPosition: 0) { finished in
                 self.state = .closed
-                self.leftViewController?.view.removeFromSuperview()
-                self.leftViewController = nil
+                self.sideViewController?.view.removeFromSuperview()
+                self.sideViewController = nil
             }
         }
     }
@@ -95,19 +105,19 @@ extension ContainerViewController: UIGestureRecognizerDelegate {
 
         switch recognizer.state {
             case .began:
-                addLeftViewController()
+                addSideViewController()
             
             case .changed:
                 let horizontalCenter = self.view.center.x
-                let movingXPosition = self.centerNavigationController.view.center.x
+                let movingXCenter = self.centerNavigationController.view.center.x
+                let movingXOrigin = self.centerNavigationController.view.frame.origin.x
                 let translation = recognizer.translation(in: view)
                 
                 let startedMovingToLeft = (translation.x < 0)
-                let isNotFurtherLeftThanScreenCenter = ((movingXPosition + translation.x) <= horizontalCenter)
+                let isNotFurtherLeftThanScreenCenter = ((movingXCenter + translation.x) <= horizontalCenter)
                 let startedMovingToRight = (translation.x > 0)
-                let isNotFurtherRightThanOffset = (movingXPosition + translation.x) >= (view.bounds.size.width - self.centerPanelExpandedOffset)
-                
-                
+                let isNotFurtherRightThanOffset = (movingXOrigin + translation.x) >= (view.bounds.size.width - self.centerPanelExpandedOffset)
+                                
                 if let movingView = self.centerNavigationController.view {
                     if (isNotFurtherLeftThanScreenCenter && startedMovingToLeft) || (isNotFurtherRightThanOffset && startedMovingToRight) {
                         break
@@ -118,16 +128,16 @@ extension ContainerViewController: UIGestureRecognizerDelegate {
                 }
             
             case .ended:
-                if let _ = leftViewController, let movingView = self.centerNavigationController.view {
+                if let _ = sideViewController, let movingView = self.centerNavigationController.view {
                     let hasMovedBeyondExpandingPoint = movingView.center.x > ((self.expandPointRatio + 1/2) * view.bounds.size.width)
                     let hasSufficientVelocityForStateChange = abs(recognizer.velocity(in: view).x) > self.sufficientVelocityForStateChange
                     let isDirectedInProperDirection = (self.state == .closed) ? recognizer.velocity(in: view).x > 0 : recognizer.velocity(in: view).x < 0
                     
                     switch self.state {
                         case .closed:
-                            animateLeftPanel(shouldExpand: (hasMovedBeyondExpandingPoint || hasSufficientVelocityForStateChange) && isDirectedInProperDirection)
+                            animateSidePanel(shouldExpand: (hasMovedBeyondExpandingPoint || hasSufficientVelocityForStateChange) && isDirectedInProperDirection)
                         case .expanded:
-                            animateLeftPanel(shouldExpand: (hasMovedBeyondExpandingPoint && !hasSufficientVelocityForStateChange) || !isDirectedInProperDirection)
+                            animateSidePanel(shouldExpand: (hasMovedBeyondExpandingPoint && !hasSufficientVelocityForStateChange) || !isDirectedInProperDirection)
                     }
                 }
             
@@ -138,14 +148,15 @@ extension ContainerViewController: UIGestureRecognizerDelegate {
     
 }
 
-
-
 private extension UIStoryboard {
     
-    static func mainStoryboard() -> UIStoryboard { return UIStoryboard(name: "Main", bundle: Bundle.main) }
+    static func mainStoryboard() -> UIStoryboard {
+        return UIStoryboard(name: "Main", bundle: Bundle.main)
+        
+    }
     
-    static func leftViewController() -> SideViewController? {
-        return mainStoryboard().instantiateViewController(withIdentifier: "LeftViewController") as? SideViewController
+    static func sideViewController() -> SideViewController? {
+        return mainStoryboard().instantiateViewController(withIdentifier: "SideViewController") as? SideViewController
     }
     
     static func centerViewController() -> CenterViewController? {
